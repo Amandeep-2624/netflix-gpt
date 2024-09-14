b01c97dee01f7109d365b2f26c0e534214b9c855
Amandeep-2624 <amandeeprewani21@gmail.com>
2024-09-14 15:01:44 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="bg-orange-400 font-extrabold text-green-600 px-4">Lets build Netflix-gpt</h2>
     </div>
   );
 }
